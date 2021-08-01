52619eeba41a7e201e010ee196ca3818214267c5
5ubstitute <74017583+5ubstitute@users.noreply.github.com>
2021-08-01 12:06:55 +0200

    Create karelsFirstProgram.java

---
+++ karelsFirstProgram.java
@@ -0,0 +1,11 @@
+void karelsFirstProgram () {
+	moveForward ();
+	pickBeeper ();
+	moveForward ();
+	turnLeft ();
+	moveForward ();
+	turnRight ();
+	moveForward ();
+	dropBeeper ();
+	moveForward ();
+}
